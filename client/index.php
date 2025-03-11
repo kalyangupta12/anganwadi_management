455d9bfa0a9b2c230ca276168726e49697d1bd0d
Arghajit Saha <sahaarghajit8@gmail.com>
2025-03-11 09:34:58 +0530

    client side ui fixes

--- client/index.php
+++ client/index.php
@@ -231,42 +231,6 @@
             </div>
           </div>
 
-          <div class="hidden md:flex items-center space-x-3">
-            <div class="relative">
-              <input
-                type="text"
-                placeholder="Search..."
-                class="pl-10 pr-4 py-2 border rounded-md text-sm focus:outline-none focus:ring-2 focus:ring-govt-blue" />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="absolute left-3 top-2.5 h-4 w-4 text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-              </svg>
-            </div>
-            <button
-              class="bg-govt-blue hover:bg-govt-dark-blue text-white px-4 py-2 rounded-md flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-4 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-              </svg>
-              Search
-            </button>
-          </div>
 
           <button class="md:hidden">
             <svg
@@ -297,31 +261,7 @@
             <a
               href="#testimonials"
               class="px-4 py-3 hover:bg-govt-dark-blue transition-colors">Schemes</a>
-            <a
-              href="#"
-              class="px-4 py-3 hover:bg-govt-dark-blue transition-colors">Resources</a>
-            <a
-              href="./attendance.php"
-              class="px-4 py-3 hover:bg-govt-dark-blue transition-colors flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-4 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M16 7a4 4 0 11-8 0 4 4 0 018 0z" />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-              </svg>
-              Attendance
-            </a>
+            
             <a
               href="#contact"
               class="px-4 py-3 hover:bg-govt-dark-blue transition-colors">Contact</a>
@@ -397,7 +337,7 @@
                 <path d="M22 10v6M2 10l10-5 10 5-10 5z" />
                 <path d="M6 12v5c0 2 1 3 3 3h6c2 0 3-1 3-3v-5" />
               </svg>
-              Student Registration
+              Pregnant Lady Resources
             </a>
             <a
               href="#"
@@ -414,7 +354,7 @@
                 <path
                   d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20" />
               </svg>
-              Download Resources
+              Lactating Women Resources
             </a>
           </div>
 
